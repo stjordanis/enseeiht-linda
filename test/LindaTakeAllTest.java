51690a48285a9cbb367c90582f909629d2d7a6b1
Perrier Matthieu <mperrier3@localhost.localdomain>
2016-12-13 18:14:51 +0100

    Testss unitaires finis

--- test/LindaTakeAllTest.java
+++ test/LindaTakeAllTest.java
@@ -2,122 +2,121 @@ package linda.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.After;
 
-import linda.*;
+import linda.Linda;
+import linda.Tuple;
 
 public class LindaTakeAllTest {
-	private Linda linda;
-	private Tuple motif;
-	private Tuple tuple;
-	private Tuple tupleVide;
-	
+	private static Linda linda;
+	private static Tuple motif;
+	private static Tuple tuple;
+	private static Tuple tupleVide;
 	
 	@BeforeClass
-	public void setUpBeforeClass() throws Exception {
-		linda = new linda.shm.CentralizedLinda();	
-        // Linda linda = new linda.server.LindaClient("//localhost:4000/aaa");
-		
+	public static void setUpBeforeClass() throws Exception {
 		motif = new Tuple(Character.class, String.class, Integer.class);
 		tuple = new Tuple('a', "toto", 4);
 		tupleVide = new Tuple();
 	}
 
-	@After
-	public void cleanUp() {
-		linda.takeAll(motif);
-		linda.takeAll(tupleVide);
-	}
-	
-	@Test
-	public void testTakeTuple() {
-		linda.write(tuple);
-		assertTrue(linda.take(tuple).equals(tuple));
+	@Before
+	public void setUp() {
+		//linda = new linda.tshm.CentralizedLinda();
+		linda = new linda.shm.CentralizedLinda();	
+        // linda = new linda.server.LindaClient("//localhost:4000/aaa");
 	}
 
-	@Test
-	public void testTakeMotif1() {
+
+	@Test(timeout=1000)
+	public void testTakeAllTuple() {
 		linda.write(tuple);
-		assertTrue(linda.take(motif).equals(tuple));
+		Collection<Tuple> result = linda.takeAll(tuple);
+		assertTrue(result.contains(tuple));
+		assertEquals(result.size(),1);
+	}
+
+	@Test(timeout=1000)
+	public void testTakeAll2Tuple() {
+		linda.write(tuple);
+		linda.write(tuple);
+		Collection<Tuple> result = linda.takeAll(tuple);
+		for(Tuple t : result) {
+			assertEquals(t, tuple);
+		}
+		assertEquals(result.size(),2);
 	}
 	
-	@Test
-	public void testTakeMotif2() {
+	@Test(timeout=1000)
+	public void testTakeAllMotif1() {
+		linda.write(tuple);
+		Collection<Tuple> result = linda.takeAll(motif);
+		assertTrue(result.contains(tuple));
+		assertEquals(result.size(),1);
+	}
+	
+	@Test(timeout=1000)
+	public void testTakeAllMotif2() {
 		linda.write(motif);
-		assertTrue(linda.take(motif).equals(motif));
+		Collection<Tuple> result = linda.takeAll(motif);
+		assertTrue(result.contains(motif));
+		assertEquals(result.size(),1);
 	}
 	
-	@Test
-	public void testTakeVide() {
-		linda.write(tupleVide);
-		assertTrue(linda.take(tupleVide).equals(tupleVide));
-	}
-	
-	@Test
-	public void testTakeRemove() {
+	@Test(timeout=1000)
+	public void testReadMotifAllMixte() {
 		linda.write(tuple);
-		linda.take(tuple);
+		linda.write(motif);
+		Collection<Tuple> result = linda.takeAll(motif);
+		assertTrue(result.contains(tuple));
+		assertTrue(result.contains(motif));
+		assertEquals(result.size(),2);
+	}
+	
+	@Test(timeout=1000)
+	public void testTakeAllVide() {
+		linda.write(tupleVide);
+		Collection<Tuple> result = linda.takeAll(tupleVide);
+		assertTrue(result.contains(tupleVide));
+		assertEquals(result.size(),1);
+	}
+	
+	@Test(timeout=1000)
+	public void testTakeAllReturnsEmpty() {
+		linda.write(motif);
+		assertTrue(linda.takeAll(tuple).size()==0);
+	}
+	
+	@Test(timeout=1000)
+	public void testTakeAllRemove() {
+		linda.write(tuple);
+		linda.takeAll(tuple);
 		assertNull(linda.tryRead(tuple));
 	}
 	
-	@Test
-	public void testTakeBlock() throws InterruptedException {
-        Thread th = 
-    	        new Thread() {
-    	            public void run() {
-    	                try {
-    	                    linda.take(new Tuple(motif));
-    	                } catch (Exception e) {
-    	                    ;
-    	                }
-    	            }
-    	        };
-            th.start();
-            Thread.sleep(2);
-            assertTrue(th.getState() == Thread.State.BLOCKED);
-	}
-
-	@Test
-	public void testTakeUnblock() throws InterruptedException {
-        Thread th = 
-    	        new Thread() {
-    	            public void run() {
-    	                try {
-    	                    assertTrue(linda.take(new Tuple(motif)).equals(tuple));
-    	                } catch (Exception e) {
-    	                    ;
-    	                }
-    	            }
-    	        };
-            th.start();
-            Thread.sleep(2);
-            linda.write(tuple);
-            Thread.sleep(2);
-            assertTrue(th.getState() == Thread.State.TERMINATED);
-	}
-	
-	// TODO : verifier que l'exception renvoyée est bien censé être NullPointerException
-	@Test(expected=NullPointerException.class)
-	public void testTakeNullThrowsException() {
-		linda.take(null);
-	}
-	
-	@Test
-	public void testTakeNullDontBlock() throws InterruptedException {
+	@Test(timeout=1000)
+	public void testTakeAllDontBlock() throws InterruptedException {
         Thread th = 
 	        new Thread() {
 	            public void run() {
 	                try {
-	                    linda.take(null);
+	                    linda.takeAll(new Tuple(motif));
 	                } catch (Exception e) {
 	                    ;
 	                }
 	            }
 	        };
         th.start();
-        Thread.sleep(2);
-        assertTrue(th.getState() == Thread.State.TERMINATED);
+        Thread.sleep(100);
+        assertEquals(th.getState(),Thread.State.TERMINATED);
+	}
+	
+	@Test(timeout=1000, expected=NullPointerException.class)
+	public void testTakeAllNullThrowsException() {
+		linda.takeAll(null);
 	}
 }
